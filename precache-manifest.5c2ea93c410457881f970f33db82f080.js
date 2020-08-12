9da5c33554899f643a33282d17ff8e2d26385a4b
Hugo DEL PIA <hugo.del-pia@gpseo.fr>
2020-08-12 12:14:05 +0200

    Updates

---
+++ precache-manifest.5c2ea93c410457881f970f33db82f080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf8604a94dc311e6c21085d27833471f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "152dedbed193f99541f4",
+    "url": "/static/css/main.4a6fc02f.chunk.css"
+  },
+  {
+    "revision": "4f5a8a783aeb9ed4fffd",
+    "url": "/static/js/2.9ce0c458.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.9ce0c458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "152dedbed193f99541f4",
+    "url": "/static/js/main.b27eef54.chunk.js"
+  },
+  {
+    "revision": "45340ce4015c8165e791",
+    "url": "/static/js/runtime-main.468594a1.js"
+  }
+]);
